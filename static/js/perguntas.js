cc0c596a665968bf4eef028e194fc3f9ee1f9303
Ig.Freitas <parasempre2710@gmail.com>
2024-09-30 15:23:23 -0300

    30/09/2024
    Este é um código do jogo da forca, constituído por HTML, CSS e JS.

---
+++ static/js/perguntas.js
@@ -0,0 +1,27 @@
+const palavras = [
+    "sigilante",
+    "código",
+    "dualidade",
+    "intimidade",
+    "apoio",
+    "contrastante",
+    "surpresas",
+    "ironia",
+   "moralidade",
+    "dinâmico",
+];
+  
+
+
+const dicas =[
+    "qual é a premissa central da série Dexter",
+    "como Dexter justifica suas ações de vigilante",
+    "quais são os principais conflitos internos de Dexter",
+    "como a perspectiva de Dexter influencia a narrativa",
+    "quem são os principais personagens secundários e qual é o papel deles",
+    "de que maneira o ambiente de Miami contribui para a história",
+    "quais são as principais reviravoltas no enredo",
+    "como o humor negro afeta a percepção da história",
+    "quais temas principais são explorados no livro",
+    "como a estrutura e o ritmo do livro afetam o envolvimento do leitor",
+];
